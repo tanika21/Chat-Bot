7a88bc8daa9546f9a0ab4969a0f2ace604cd313d
tanika21 <31179279+tanika21@users.noreply.github.com>
2021-06-19 21:31:00 +0530

    Add files via upload

---
+++ basic chatbot.py
@@ -0,0 +1,117 @@
+
+import random
+import re
+
+bot_template = "BOT : {0}"
+user_template = "USER : {0}"
+
+def send_message(message):
+    # Print user_template including the user_message
+    print(user_template.format(message))
+    # Get the bot's response to the message
+    response = respond(message)
+    # Print the bot template including the bot's response.
+    print(bot_template.format(response))
+
+rules = {'I want (.*)': ['What would it mean if you got {0}',
+  'Why do you want {0}',
+  "What's stopping you from getting {0}"],
+ 'do you remember (.*)': ['Did you think I would forget {0}',
+  "Why haven't you been able to forget {0}",
+  'What about {0}',
+  'Yes .. and?',
+  'Yes, I remember {0}'],
+ 'do you think (.*)': ['if {0}? Absolutely.', 'No chance'],
+ 'if (.*)': ["Do you really think it's likely that {0}",
+  'Do you wish that {0}',
+  'What do you think about {0}',
+  'Really--if {0}']}
+
+def match_rule(rules, message):
+    response, phrase = "Sorry I can't help you", None
+    
+    # Iterate over the rules dictionary
+    for pattern, responses in rules.items():
+        # Create a match object
+        match = re.search(pattern,message)
+        if match is not None:
+            # Choose a random response
+            response = random.choice(responses)
+            if '{0}' in response:
+                phrase = match.group(1)
+    # Return the response and phrase
+    return response, phrase
+
+def replace_pronouns(message):
+
+    message = message.lower()
+    if 'me' in message:
+        # Replace 'me' with 'you'
+        return re.sub('me', 'you', message)
+    if 'my' in message:
+        # Replace 'my' with 'your'
+        return re.sub('my', 'your', message)
+    if 'your' in message:
+        # Replace 'your' with 'my'
+        return re.sub('your', 'my', message)
+    if 'you' in message:
+        # Replace 'you' with 'me'
+        return re.sub('you', 'me', message)
+
+    return message
+
+
+
+# Define find_name()
+def find_name(message):
+    name = None
+    # Create a pattern for checking if the keywords occur
+    name_keyword = re.compile('name|call')
+    # Create a pattern for finding capitalized words
+    name_pattern = re.compile('[A-Z]{1}[a-z]*')
+    if name_keyword.search(message):
+        # Get the matching words in the string
+        name_words = name_pattern.findall(message)
+        if len(name_words) > 0:
+            # Return the name if the keywords are present
+            name = ' '.join(name_words)
+    return name
+
+# Define respond()
+def respond(message):
+    # Find the name
+    name = find_name(message)
+    response, phrase = match_rule(rules, message)
+    if name is None:
+      if '{0}' in response:
+        # Replace the pronouns in the phrase
+        phrase = replace_pronouns(phrase)
+        # Include the phrase in the response
+        response = response.format(phrase)
+      return response
+    else:
+      return "Hello, {0}!".format(name)
+
+# Send messages
+send_message("my name is David Copperfield")
+send_message("call me Ishmael")
+send_message("People call me Cassandra")
+
+# Send the messages
+send_message("do you remember your last birthday")
+send_message("do you think humans should be worried about AI")
+send_message("I want a robot friend")
+send_message("what if you could be anything you wanted")
+
+
+send_message("do you remember Tanika's birthday?")
+
+send_message("whats your name?")
+
+send_message("what is love?")
+
+
+
+
+
+
